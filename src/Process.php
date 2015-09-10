f65c75cee1b34855f6ee85d8166204c853c47543
Viacheslav Poturaev <vearutop@gmail.com>
2015-09-10 18:03:05 +0700

    pschart initial

--- src/Process.php
+++ src/Process.php
@@ -10,6 +10,12 @@ class Process
     public $started;
     public $command;
 
+    public $avgCpuPercent;
+    public $avgMemPercent;
+    public $avgRss;
+
+    public $count;
+
     public function getShortName() {
         $l = explode(' ', $this->command, 2);
         return basename($l[0]) . ':' . $this->pid;
